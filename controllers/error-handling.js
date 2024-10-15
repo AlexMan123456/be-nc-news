e8e6156800e7172ce31556e6a035cd1788447184
AlexMan123456 <alex.man03888@gmail.com>
2024-10-15 11:49:22 +0100

    Added PATCH method to /api/articles/:article_id

--- controllers/error-handling.js
+++ controllers/error-handling.js
@@ -6,6 +6,9 @@ function sqlErrors(err, request, response, next){
     if(err.code === "22P02"){
         response.status(400).send({message: "Bad request"})
     }
+    if(err.code === "23502"){
+        response.status(400).send({message: "One or more properties must not be null"})
+    }
     if(err.code === "23503"){
         response.status(404).send({message: "Not found"})
     }
