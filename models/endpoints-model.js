41a543d90026d684421664145d054e0c499a1666
AlexMan123456 <alex.man03888@gmail.com>
2024-11-04 00:12:43 +0000

    Slight refactor in endpoints model to remove unnecessary variable declaration

--- models/endpoints-model.js
+++ models/endpoints-model.js
@@ -2,8 +2,7 @@ const fs = require("fs/promises")
 
 function fetchAllEndpoints(){
     return fs.readFile(`./endpoints.json`, "utf-8").then((fileContents) => {
-        const endpoints = JSON.parse(fileContents)
-        return endpoints
+        return JSON.parse(fileContents)
     })
 }
 
